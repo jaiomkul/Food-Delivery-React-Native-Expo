5ad8524541f30e452e73b66107e5c19cabc3cb64
jaiomkul <jaygupta2601@gmail.com>
2022-09-08 15:46:42 +0530

    setup

---
+++ App.js
@@ -0,0 +1,18 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StyleSheet, Text, View } from "react-native";
+import { TailwindProvider } from "tailwindcss-react-native";
+import HomeScreen from "./screens/HomeScreen";
+export default function App() {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <TailwindProvider>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </TailwindProvider>
+    </NavigationContainer>
+  );
+}
